6d0c05e6d875d1e44020ecf114f1b2d3487dfb09
= <=>
2022-10-19 11:47:07 -0400

    Added basic post route to add to DB

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mysql = require('mysql');
+const cors = require('cors');
 const PORT = 3002;
 const db = mysql.createConnection({
     host: 'localhost',
@@ -16,14 +17,16 @@ db.connect(err => {
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
-
-app.get('/', (req, res) => {
-    res.send('doot');
-});
-
-app.post('/addVeterinarian', (req, res) => {
-
+app.post('/addVet', (req, res) => {
+    const data = req.body;
+    let sql = 'INSERT INTO veterinarians SET ?';
+    let query = db.query(sql, data, (err, result) => {
+        if(err) throw err;
+        console.log(result);
+        res.json({response: 'got it'});
+    })
 })
 
 app.listen(PORT, () => console.log(`listening on port ${PORT}`));
