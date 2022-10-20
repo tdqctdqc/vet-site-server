7e7eca28a99b1778bce91593c1e143e1e083a9f6
= <=>
2022-10-20 12:00:15 -0400

    Added add client route

--- vet/vetRouter.js
+++ vet/vetRouter.js
@@ -5,17 +5,7 @@ const provinces = require('../provinces');
 const nameCheck = /^[A-Za-z ]{2,255}$/;
 const cityCheck = /^[A-Za-z ]{2,255}$/;
 
-vetRouter.get('/:id', async (req, res) => {
-    const id = req.params.id;
-    let sql = `SELECT * FROM veterinarians WHERE id=${id}`;
-    const connection = getConnection();
-    let query = connection.query(sql, (err, result) => {
-        connection.end();
-        if(err) throw err;
-        res.json(result);
-    });
 
-});
 
 
 vetRouter.post('/addVet', verifyAdd, async (req, res) => {
@@ -29,9 +19,36 @@ vetRouter.post('/addVet', verifyAdd, async (req, res) => {
         res.json({response: 'got it'});
     });
 });
+vetRouter.get('/:id', async (req, res) => {
+    const id = req.params.id;
+    let sql = `SELECT * FROM veterinarians WHERE id=${id}`;
+    const connection = getConnection();
+    let query = connection.query(sql, (err, result) => {
+        connection.end();
+        if(err) throw err;
+        res.json(result);
+    });
+});
+
+vetRouter.post('/:id/addClient', verifyAdd, async (req, res) => {
+    const data = req.body;
+    const id = req.params.id;
+    data['veterinarian'] = id;
+
+
+    console.log('adding client for vet ' + id);
+
+    let sql = 'INSERT INTO clients SET ?';
+    const connection = getConnection();
+    let query = connection.query(sql, data, (err, result) => {
+        connection.end();
+        if(err) throw err;
+    });
+});
 
 function verifyAdd(req, res, next) {
     const data = req.body;
+    console.log(data);
     if(!data.name || !nameCheck.test(data.name)) {
         throw 'Submitted name is not valid.';
     }
